2155dee912892511ad72b337d42aea2a64b65582
Martin Pettersson <martin_pettersson@outlook.com>
2021-02-24 22:10:37 +0100

    Allow multiple types

--- test/TypeValidation.spec.js
+++ test/TypeValidation.spec.js
@@ -46,5 +46,22 @@ describe("TypeValidation", () => {
             expect(() => validation.validate(new Date(), "object")).to.throw(ValidationError);
             expect(() => validation.validate(null, "object")).to.throw(ValidationError);
         });
+
+        it("should allow multiple types", () => {
+            validation.validate("",  "number", "date", "string");
+            validation.validate(new Date(),  "number", "date", "string");
+        });
+
+        it("should throw error if no types matches", () => {
+            expect(() => validation.validate("", "number", "date", "null")).to.throw(ValidationError);
+            expect(() => validation.validate([], "number", "date", "null")).to.throw(ValidationError);
+        });
+
+        it("should include all type options in error message", () => {
+            expect(() => validation.validate("", "number")).to.throw("Must be of type number");
+            expect(() => validation.validate("", "number", "date")).to.throw("Must be of type number or date");
+            expect(() => validation.validate("", "number", "date", "null")).to.throw("Must be of type number, date or null");
+            expect(() => validation.validate("", "number", "date", "null", "array")).to.throw("Must be of type number, date, null or array");
+        });
     });
 });
